3d2c730e4a0784eda1639bce367505f13fefa564
Moeez Ismail <moeezaliismaiahmad@gmail.com>
2026-04-03 17:33:47 +0500

    making changes for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const session = require('express-session');
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env?.FRONTEND_URL,
     credentials: true,
 }));
 app.use(express.json());
